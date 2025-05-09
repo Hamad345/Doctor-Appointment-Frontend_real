3a7c760dd62987c3e2820e96d390a68e45ddb4f8
HamadIslamian <hamad.yousafzai34@gmail.com>
2025-05-09 12:35:01 +0500

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ import Login from'./pages/Login'
 import About from'./pages/About'
 import Contact from'./pages/Contact'
 import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Navbar from './components/Navbar'
+import Appointments from './pages/Appointments'
 import { Routes,Route } from 'react-router'
 function App() {
  return (
     <>
        <div className='mx-4 sm:mx-[10%]'> 
+         <Navbar  />
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/doctors' element={<Doctors/>} />
@@ -17,7 +21,9 @@ function App() {
           <Route path='/login' element={<Login/>} />
           <Route path='/about' element={<About/>} />
           <Route path='/contact' element={<Contact/>} />
-           <Route path='/my-profile' element={<MyProfile/>} />
+         <Route path='/my-profile' element={<MyProfile/>} />
+         <Route path='/my-appointments' element={<MyAppointments/>} />
+         <Route path='/appointments/:docId' element={<Appointments/>} />
         </Routes>
      
        </div>
